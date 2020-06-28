e22253abd503386769d0afe70e370844e226fc07
fatemehpiri <58412311+fatemehpiri@users.noreply.github.com>
2020-06-28 12:55:38 +0430

    Update App.js

--- Project/Implement/src/App.js
+++ Project/Implement/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import "./components/NavBar.css";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/Login";
