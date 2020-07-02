39b034c11c79b83a5254e70ae6b3b327eb5c0d76
Soroor <aghamiri.soroor@gmail.com>
2020-07-02 11:03:12 +0430

    Added shadow to the navbar
    Will compelete it next time :sweat_smile:

--- Project/Implement/src/components/StudentPanel.js
+++ Project/Implement/src/components/StudentPanel.js
@@ -6,18 +6,26 @@ import "./NavBar.css";
 const StudentPanel = () => {
   return (
     <div>
-      <nav className="navbar navbar-light topLabel">
-        <img
-          src={require("./Group_128.png")}
-          id="logoIcon"
-          alt="lamp1"
-          loading="lazy"
-        />
-        <span className="navbar-brand" id="LAMP">
-          LAMP
-        </span>
-      </nav>
+      <div className="topLabel_Wrapper">
+        <nav className="navbar navbar-light" id="topLabel">
+          <img
+            src={require("./Group_128.png")}
+            id="logoIcon"
+            alt="lamp1"
+            loading="lazy"
+          />
+          <span className="navbar-brand" id="LAMP">
+            LAMP
+          </span>
+          <div id="Search_Box">
+            <span id="Search_Text">I'm searching for</span>
+          </div>
+        </nav>
+      </div>
+
+      {/* <div className="NavBar_Wrapper"> */}
       <StudentNB />
+      {/* </div> */}
     </div>
   );
 };
