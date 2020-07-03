218e85acfcf214b7d95e600f8d53ff93efc8665f
Soroor <aghamiri.soroor@gmail.com>
2020-07-03 23:48:54 +0430

    Student Panel completed
    Search box function must be added in next sprint

--- Project/Implement/src/components/StudentPanel.js
+++ Project/Implement/src/components/StudentPanel.js
@@ -17,15 +17,34 @@ const StudentPanel = () => {
           <span className="navbar-brand" id="LAMP">
             LAMP
           </span>
+
           <div id="Search_Box">
-            <span id="Search_Text">I'm searching for</span>
+            <input
+              type="text"
+              id="Search_Text"
+              name="SearchBox"
+              placeholder="I'm searching for"
+            />
+          </div>
+
+          <div className="divider">
+            <svg id="devider_Line" viewBox="0 0 1 30">
+              <path d="M 0 0 L 0 30" />
+            </svg>
+          </div>
+          <div className="profile_Indicator">
+            <img
+              src={require("../Images/Ellipse.png")}
+              id="profile_Pic"
+              alt="picture"
+              loading="lazy"
+            />
+            <div id="profile_Name">Jack</div>
           </div>
         </nav>
       </div>
 
-      {/* <div className="NavBar_Wrapper"> */}
       <StudentNB />
-      {/* </div> */}
     </div>
   );
 };
