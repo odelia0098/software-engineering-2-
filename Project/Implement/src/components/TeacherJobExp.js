d2c29e228463117d7fb3993086f2035125990221
fatemehpiri <58412311+fatemehpiri@users.noreply.github.com>
2020-07-24 10:45:50 +0430

    Teacher's job experiences Added.

--- Project/Implement/src/components/TeacherJobExp.js
+++ Project/Implement/src/components/TeacherJobExp.js
@@ -6,7 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-//import DialogContentText from "@material-ui/core/DialogContentText";
+
 import DialogTitle from "@material-ui/core/DialogTitle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -116,14 +116,11 @@ const Job = () => {
       />
 
       <form>
-        {/* */}
-
         <div className="personal-forms">
           <div className="forms-names">
             <span>Job Experience</span>
           </div>
 
-          {/* <Plus fontSize="large" color="action" /> */}
           <div className={classes.root}>
             <Icon
               className="addIcons"
