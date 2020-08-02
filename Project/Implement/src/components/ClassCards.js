046046ce58cac4d97075af78e4b5b299237790c4
fatemehpiri <58412311+fatemehpiri@users.noreply.github.com>
2020-08-02 09:24:04 +0430

    ClassCards Added.

---
+++ Project/Implement/src/components/ClassCards.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 300,
+    marginRight: 50,
+    marginBottom: 100,
+    display: "inline-block"
+  },
+  media: {
+    height: 350
+  }
+});
+
+const MiniCard = props => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          // style={{ height: 0, paddingTop: "56.25%" }}
+
+          image={props.image}
+          title="Contemplative Reptile"
+        ></CardMedia>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.course}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            grade: {props.grade}
+            <br />
+            by {props.instructor}
+            <br />
+            price: {props.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button
+          style={{
+            backgroundColor: "rgba(255, 90, 135, 1)",
+            maxWidth: "280px",
+            minWidth: "280px",
+            color: "white"
+          }}
+        >
+          Register
+        </Button>
+        {/* <button style={{ backgroundColor: "rgba(255, 90, 135, 1)" }}>
+          Register
+        </button> */}
+      </CardActions>
+    </Card>
+  );
+};
+
+export default MiniCard;
